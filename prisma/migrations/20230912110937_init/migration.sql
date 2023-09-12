f6f86d56b216f0b5995bc83a7c2eb2f2b66b4c2c
Cyril Vrousos <cyril.vrousos@etu.unice.fr>
2023-09-12 16:25:05 +0200

    Update

--- prisma/migrations/20230912110937_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "VisitCounter" (
-    "id" SERIAL NOT NULL,
-    "value" INTEGER NOT NULL DEFAULT 0,
-
-    CONSTRAINT "VisitCounter_pkey" PRIMARY KEY ("id")
-);
--- src/server.js
+++ src/server.js
@@ -6,23 +6,31 @@ const port = 8080;
 
 const prisma = new PrismaClient();
 
-const requestListener = async (req, res) => {
+const requestListener = async function(req, res) {
     try {
-        const [visitCounter] = await prisma.visitCounter.upsert({
-            where: { id: 1 },
-            create: {
-                id: 1,
-                value: 1,
-            },
-            update: {
-                value: {
-                    increment: 1,
+        const existingVisitCounter = await prisma.visitCounter.findUnique({ where: { id: 1 } });
+        
+        if (!existingVisitCounter) {
+            await prisma.visitCounter.create({
+                data: {
+                    id: 1,
+                    value: 1,
                 },
-            },
-        });
-
-        res.writeHead(200);
-        res.end(`Hello World from Node.js HTTP Server\nVisits: ${visitCounter.value}`);
+            });
+            
+            res.writeHead(200);
+            res.end(`Hello World from Node.js HTTP Server\nVisits: 1`);
+        } else {
+            const newValue = existingVisitCounter.value + 1;
+            
+            await prisma.visitCounter.update({
+                where: { id: 1 },
+                data: { value: newValue },
+            });
+            
+            res.writeHead(200);
+            res.end(`Hello World from Node.js HTTP Server\nVisits: ${newValue}`);
+        }
     } catch (error) {
         console.error("Error:", error);
         res.writeHead(500);
